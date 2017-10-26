6038913269800d5334e39ade99bf9d5cf22a8149
Bill Piel <bill@billpiel.com>
2017-10-26 08:41:54 -0400

    org-conf org-metaleft org-metaright

--- conf/org-conf.el
+++ conf/org-conf.el
@@ -2,3 +2,5 @@
 
 (define-key org-mode-map (kbd "M-N") 'org-metadown)
 (define-key org-mode-map (kbd "M-P") 'org-metaup)
+(define-key org-mode-map (kbd "M-[") 'org-metaleft)
+(define-key org-mode-map (kbd "M-]") 'org-metaright)
