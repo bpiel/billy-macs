7abf066bc7dc6a4f524126835c82d57f78e15960
Bill Piel <bill@billpiel.com>
2023-12-06 04:46:43 -0500

    set ido-default-buffer-method to selected-window

--- init.el
+++ init.el
@@ -253,6 +253,7 @@ current buffer is not visiting a file."
      (mark " "
 	   (name 16 -1)
 	   " " filename)))
+ '(ido-default-buffer-method 'selected-window)
  '(package-selected-packages
    '(go-mode cider clojure-mode company flycheck lsp-mode rustic lsp-java ccls json-mode avy pdf-tools use-package vlf smex paredit idomenu flx-ido edn browse-kill-ring better-defaults ac-cider))
  '(rust-rustfmt-bin "/home/bill/.cargo/bin/rustfmt")
