b61bb720c5fff6bf529eaa5c1565e2b734833182
Bill Piel <bill@billpiel.com>
2025-02-21 08:22:26 -0500

    update clojure-mode, cider

---
+++ elpa/transient-20250221.106/transient-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "transient" "20250221.106"
+  "Transient commands."
+  '((emacs  "26.1")
+    (compat "30.0.0.0")
+    (seq    "2.24"))
+  :url "https://github.com/magit/transient"
+  :commit "3cd02ec21cedb70ce1ed8e87b6c5cf1557a59cbe"
+  :revdesc "3cd02ec21ced"
+  :keywords '("extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
